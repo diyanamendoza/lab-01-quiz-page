1cde797efda2398a56d56527b9beb17084f914f7
diyanamendoza <diyana.amp@pm.me>
2021-08-16 14:50:10 -0700

    Add test

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,19 @@
-import './example.test.js';
+import { countsAsAYes } from "../utils.js";
+
+const test = QUnit.test;
+
+test('countsAsAYes takes in Yes and returns true', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const testResponse = 'Yes';
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes(testResponse);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
